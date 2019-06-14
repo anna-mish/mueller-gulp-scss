d91abb91dcdd8133a069a2d1eaf324d0ae6705b5
anna-mish <anna-mish@users.noreply.github.com>
2019-06-14 14:02:37 +0300

    add block partner

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,9 +4,34 @@ moment.locale('ru');
 
 const onPageLoaded = () => {
     setInterval(ticker, 1000);
-     
-};
+    
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+      });
 
+    
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          320:{
+              items:1
+          },
+          767:{
+              items:2
+          },
+          992:{
+              items:3
+          }
+      }
+    })
+    
 const ticker = () => {
     $('#ticker').html(moment().format('dddd, do MMMM YYYY --- hh:mm:ss'));
 };
