d1795ffa6f5e36c1cc906c2bb1cac199e21ac9bc
znova.roman <roman.znova@omnigon.com>
2019-06-14 14:56:38 +0300

    update swiper carousel

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,37 +1,35 @@
 import $ from './libs/jquery'
 import moment from './libs/moment';
-moment.locale('ru');
+import Swiper from './libs/swiper';
 
 const onPageLoaded = () => {
-    setInterval(ticker, 1000);
-    
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
         spaceBetween: 30,
         pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+            el: '.swiper-pagination',
+            clickable: true,
         },
-      });
+    });
 
-    
-    $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-          320:{
-              items:1
-          },
-          767:{
-              items:2
-          },
-          992:{
-              items:3
-          }
-      }
-    })
-    
+
+    // $('.owl-carousel').owlCarousel({
+    //     loop: true,
+    //     margin: 10,
+    //     nav: true,
+    //     responsive: {
+    //         320: {
+    //             items: 1
+    //         },
+    //         767: {
+    //             items: 2
+    //         },
+    //         992: {
+    //             items: 3
+    //         }
+    //     }
+    // })
+};
 const ticker = () => {
     $('#ticker').html(moment().format('dddd, do MMMM YYYY --- hh:mm:ss'));
 };
